196c7fa8f9bddec414adca89febc72863e3c4957
Astral Shop <miur666@gmail.com>
2026-09-04 23:31:21 -0500

    Expose supabase client to the global window object

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -4,3 +4,4 @@ const SUPABASE_URL = "https://crlqxvqnjevgtgcchbsz.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNybHF4dnFuamV2Z3RnY2NoYnN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1NzMyMDgsImV4cCI6MjEwNDE0OTIwOH0.-o2u0qWsTlig8bHwBiPn0P-iOa3vKFlBs_TkUtCM81k";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+window.supabase = supabase;
